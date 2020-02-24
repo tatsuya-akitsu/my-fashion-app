0ad0f65183c9a5407c352aa343892f3651288c32
秋津 辰弥 <tatsuya.regulus0138@gmail.com>
2020-02-24 22:48:27 +0900

    feature: deploy

---
+++ store/env.js
@@ -0,0 +1,13 @@
+export const E = {
+  API_KEY: 'AIzaSyB41KL5ognkglLnE9hsiPgml4Xh4nxwGxs',
+  AUTH_DOMAIN: 'my-weather-fasion.firebaseapp.com',
+  DATABASE_URL: 'https://my-weather-fasion.firebaseio.com',
+  PROJECT_ID: 'my-weather-fasion',
+  STORAGE_BUCKET: 'my-weather-fasion.appspot.com',
+  MESSAGING_SENDER_ID: '762600376692',
+  WEATHER_API_KEY: 'c54dcb9986005923e129ec43164a4948',
+  AJAX_GET_WEATHER_DATA:
+    'https://api.openweathermap.org/data/2.5/forecast?q={city},jp&units=metric&lang=ja&APPID={api}',
+  AJAX_GET_TRANSLATE_CITY:
+    'https://script.google.com/macros/s/AKfycbwBcMTPNNKWAqWrpMkBY7d2hszSh1_QA370QV7ScnBCMZkwtKAv/exec?text={city}&source=ja&target=en'
+}
